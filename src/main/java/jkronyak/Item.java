248dbd6601d8afd3567fe1ccd4d3f4562eeb5c13
jkronyak <56709613+jkronyak@users.noreply.github.com>
2021-08-14 19:42:41 -0400

    massive overhaul: -two constructors -getters and setters -wrapper datatypes for instance variables

--- src/main/java/jkronyak/Item.java
+++ src/main/java/jkronyak/Item.java
@@ -1,12 +1,54 @@
 package jkronyak;
 
-public class Item {
+import java.io.Serializable;
 
-    private String plu;
+public class Item implements Serializable {
+
+    private Integer plu;
     private String name;
-    private double price;
+    private Double price;
 
-    public String toString(){ return name; }
+    public Item(Integer plu, String name, Double price){
+        this.plu = plu;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Item(String name, Double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public Integer getPlu() {
+        return plu;
+    }
+
+    public void setPlu(Integer plu) {
+        this.plu = plu;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+
+    @Override
+    public String toString(){
+        return name + "\t\t\t" + price;
+    }
 
 
 
