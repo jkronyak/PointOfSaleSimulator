d0a6a8609f05b82e8fe52f4c02fed31c72341bf5
jkronyak <56709613+jkronyak@users.noreply.github.com>
2021-08-23 17:11:31 -0400

    Closed resources.

--- src/main/java/jkronyak/ItemDAO.java
+++ src/main/java/jkronyak/ItemDAO.java
@@ -12,13 +12,17 @@ public class ItemDAO implements DAO<Item> {
     public Item get(Integer plu) {
         Connection connection = MySQLConnection.getConnection();
         try {
+
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM itemdb.items WHERE plu=" + plu);
 
             if(resultSet.next()) {
-                return new Item(resultSet.getInt("plu"), resultSet.getString("name"), resultSet.getDouble("price"));
+                return new TransactionItem(resultSet.getInt("plu"), resultSet.getString("name"), resultSet.getDouble("price"));
             }
 
+            connection.close();
+            statement.close();
+            resultSet.close();
         } catch (SQLException e) {
             System.out.println("SQLException: " + e.getMessage());
             System.out.println("SQLState: " + e.getSQLState());
