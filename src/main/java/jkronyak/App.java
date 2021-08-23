a9375cb8065983dc005bc0b4cb3fdf5c276759c2
jkronyak <56709613+jkronyak@users.noreply.github.com>
2021-08-23 17:12:28 -0400

    Set window title

--- src/main/java/jkronyak/App.java
+++ src/main/java/jkronyak/App.java
@@ -18,6 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setTitle("Point Of Sale");
         stage.setScene(scene);
         stage.show();
 
