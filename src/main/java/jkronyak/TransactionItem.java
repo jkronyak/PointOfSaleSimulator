ce0ac352ae4e3a4813e90a2e2873d230a05d47a3
jkronyak <56709613+jkronyak@users.noreply.github.com>
2021-08-23 17:11:16 -0400

    Added TransactionItem class. Extends Item class and is used to hold quantities for a Transaction.

---
+++ src/main/java/jkronyak/TransactionItem.java
@@ -0,0 +1,40 @@
+package jkronyak;
+
+import java.util.Objects;
+
+public class TransactionItem extends Item {
+
+    private Integer quantity;
+
+
+    public TransactionItem(Integer plu, String name, Double price, Integer quantity) {
+        super(plu, name, price);
+        this.quantity = quantity;
+    }
+
+    public TransactionItem(Integer plu, String name, Double price) {
+        super(plu, name, price);
+        this.quantity = 1;
+    }
+
+    public Integer getQuantity() { return quantity; }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TransactionItem that = (TransactionItem) o;
+        return Objects.equals(super.getPlu(), that.getPlu());
+    }
+
+    @Override
+    public Double getPrice() {
+        return quantity * super.getPrice();
+    }
+
+    public String toString() {
+        return super.getName() + "\t\t" + quantity + "@" + super.getPrice() + "\t\t" + getPrice();
+    }
+}
